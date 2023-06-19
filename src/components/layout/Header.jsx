725f72c0d0be8879ebb52d8f09a16fb6f47f6253
Irfnmdlptra <bangasrat@gmail.com>
2023-06-19 15:05:34 +0800

    asa

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,16 @@
 export default function Header(props) {
-    return(
-        <>
-        
-        </>
-    )
+  return (
+    <>
+      <div className="row">
+        <div className="col-12 text-center">
+          <h1 className="fw-bold">FEEDUITEN APPS</h1>
+          <hr className="w-75 mx-auto" />
+          <h2 className="fw-bold">Rp. {props.sisa} ,</h2>
+          <span className="title-sm">
+            Sisa uang kamu tersisa {props.persen}% lagi
+          </span>
+        </div>
+      </div>
+    </>
+  );
 }
