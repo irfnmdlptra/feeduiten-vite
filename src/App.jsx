3da71c4bdeba5c66817e04ec080367885ce994f4
Irfnmdlptra <bangasrat@gmail.com>
2023-06-19 15:17:41 +0800

    as

--- src/App.jsx
+++ src/App.jsx
@@ -99,8 +99,8 @@ const App = () => {
       <Header sisa={sisaUang.toLocaleString()} persen={persentaseUang} />
 
       <div className="row mt-4">
-        <SummaryCard title="Pemasukan" nominal={pemasukanUang.toLocaleString()} transaksi={transaksiIN}/>
-        {/* <div className="col-6">
+        {/* <SummaryCard title="Pemasukan" nominal={pemasukanUang.toLocaleString()} transaksi={transaksiIN}/> */}
+        <div className="col-6">
           <div className="card-wrapper p-4">
             <div className="icon-wrapper-in box-sh">
               <i className="bi bi-wallet2" aria-hidden="true"></i>
@@ -112,7 +112,7 @@ const App = () => {
               <span className="title-sm"> Transaksi</span>
             </div>
           </div>
-        </div> */}
+        </div>
 
         <div className="col-6">
           <div className="card-wrapper p-4 ">
